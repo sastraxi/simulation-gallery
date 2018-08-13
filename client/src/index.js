204936e1f1561efb251a5f80f4212c9b4077a968
Cameron Gorrie <sastraxi@gmail.com>
2018-08-12 23:34:41 -0400

    eslint up in this maw

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
-import "./index.css";
+import './index.css';
 
-import * as Babylon from "babylonjs";
+import * as Babylon from 'babylonjs';
 import { Vec3 } from './util';
 
-const canvas = document.getElementById("game");
+const canvas = document.getElementById('game'); // eslint-disable-line no-undef
 const engine = new Babylon.Engine(canvas, true);
 
 // This creates a basic Babylon Scene object (non-mesh)
 const scene = new Babylon.Scene(engine);
 
 // This creates and positions a free camera (non-mesh)
-const camera = new Babylon.FreeCamera("camera1", new Vec3(0, 5, -10), scene);
+const camera = new Babylon.FreeCamera('camera1', new Vec3(0, 5, -10), scene);
 
 // This targets the camera to scene origin
 camera.setTarget(Vec3.Zero());
@@ -19,25 +19,25 @@ camera.setTarget(Vec3.Zero());
 camera.attachControl(canvas, true);
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-const light = new Babylon.HemisphericLight("light1", new Vec3(0, 1, 0), scene);
+const light = new Babylon.HemisphericLight('light1', new Vec3(0, 1, 0), scene);
 
 // Default intensity is 1. Let's dim the light a small amount
 light.intensity = 0.7;
 
 // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-const sphere = Babylon.Mesh.CreateSphere("sphere1", 16, 2, scene);
+const sphere = Babylon.Mesh.CreateSphere('sphere1', 16, 2, scene);
 
 // Move the sphere upward 1/2 its height
 sphere.position.y = 1;
 
 // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-const ground = Babylon.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+const ground = Babylon.Mesh.CreateGround('ground1', 6, 6, 2, scene); // eslint-disable-line no-unused-vars
 
-engine.runRenderLoop(function() {
+engine.runRenderLoop(() => {
   scene.render();
 });
 
 // Resize the engine on window resize
-window.addEventListener('resize', function() {
+window.addEventListener('resize', () => { // eslint-disable-line no-undef
   engine.resize();
 });
--- client/src/util.js
+++ client/src/util.js
@@ -1,3 +1,4 @@
-import * as Babylon from "babylonjs";
+/* eslint-disable import/prefer-default-export */
+import * as Babylon from 'babylonjs';
 
 export const Vec3 = Babylon.Vector3;
